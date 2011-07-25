431ca3c7b5007290981a91d86b3702b96f6dff06
Mike reDD <mikered@hotmail.com>
2011-07-25 00:28:37 -0700

    modified:   wmii/scripts/checktemp.sh

--- wmii/scripts/checktemp.sh
+++ wmii/scripts/checktemp.sh
@@ -13,10 +13,10 @@ GFX="$(nvidia-settings -q GPUCoreTemp | grep '):' | awk '{print $4}' | cut -f 1 
 # Harddisk
 GETHDTEMP="$(nc localhost 7634 | sed 's|dev|\n|g')"
 echo -e "$GETHDTEMP" > $HOME/.hddtemp.txt
-HDA="$(cat ~/.hddtemp.txt | grep "sda" | tr -d '/' | sed 's/sda|WDC WD15EARS-00MVWB0//g' | cut -f2 -d "|")"
-HDB="$(cat ~/.hddtemp.txt | grep "sdb" | tr -d '/' | sed 's/sdb|Hitachi HDP725032GLA360//g' | cut -f2 -d "|")"
-HDC="$(cat ~/.hddtemp.txt | grep "sdc" | tr -d '/' | sed 's/sdc|WDC WD10EACS-00D6B1//g' | cut -f2 -d "|")"
-HDD="$(cat ~/.hddtemp.txt | grep "sdd" | tr -d '/' | sed 's/sdd|WDC WD10EACS-00D6B1//g' | cut -f2 -d "|")"
+HDA="$(cat $HOME/.hddtemp.txt | grep "sda" | tr -d '/' | sed 's/sda|WDC WD15EARS-00MVWB0//g' | cut -f2 -d "|")"
+HDB="$(cat $HOME/.hddtemp.txt | grep "sdb" | tr -d '/' | sed 's/sdb|Hitachi HDP725032GLA360//g' | cut -f2 -d "|")"
+HDC="$(cat $HOME/.hddtemp.txt | grep "sdc" | tr -d '/' | sed 's/sdc|WDC WD10EACS-00D6B1//g' | cut -f2 -d "|")"
+HDD="$(cat $HOME/.hddtemp.txt | grep "sdd" | tr -d '/' | sed 's/sdd|WDC WD10EACS-00D6B1//g' | cut -f2 -d "|")"
 
 # 
 echo -e "C0: $COREONE° C1: $CORETWO° C2: $CORETHREE° C3: $COREFOUR° Gfx: $GFX° HDA: $HDA° HDB: $HDB° HDC: $HDC° HDD: $HDD°" | wmiir create /rbar/achecktemp
