b26bf325420b275ce0ec07ff5a696b871433118b
Mike Redd <mikeredd@gmail.com>
2016-07-02 10:38:17 -0500

    update my nowplaying script.  Changes to be committed: 	modified:   i3/scripts/nowplaying.bash

--- i3/scripts/nowplaying.bash
+++ i3/scripts/nowplaying.bash
@@ -1,35 +1,68 @@
-#!/bin/bash
-#---------------------------------------------
-# file:     ~/.i3/scripts/nowplaying.bash
-# author:   Mike Redd - http://m-redd.com/
-# vim:fenc=utf-8:nu:ai:si:et:ts=4:sw=4:ft=sh:
-#---------------------------------------------
-
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: nowplaying
+#
+#         USAGE: ./nowplaying
+#
+#   DESCRIPTION: Now Playing script helps to display now playing info from
+#                MOC, MPlayer, VLC, LastFM, Pandora and Spotify.
+#                ( ie: LastFM - Shell-FM, Pandora - Pianobar )
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: bash, moc(Music On Console), MPlayer, Pianobar(Pandora, Shell-FM(LastFM), Spotify
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Mike Redd (MikereDD), mredd@0tue0.com
+#  ORGANIZATION: Dumb Terminal Team
+#       CREATED: 06/03/2012 05:27:57 PM PDT
+#      REVISION: 07/02/2016 10:35:20 PM PDT
+#===============================================================================
+#
 # Volume
-VOLUME=$(sh $HOME/.wmii/scripts/vol.sh)
-VOLUMET=$(sh $HOME/.wmii/scripts/vol-bar.sh)
+VOLUME=$(sh $HOME/scripts/audio/vol.sh)
+VOLUMET=$(sh $HOME/scripts/audio/vol-bar.sh)
 
 # MPlayer
 IFMPON=`sh $HOME/scripts/system/check_mplayer_running-PID.sh`
 if [ "$IFMPON" = "On" ]; then
-   echo -e "▶ $(sh ~/scripts/video/nowplaying-mplayer.sh)"
+   echo -e "►$(sh ~/scripts/video/nowplaying-mplayer.sh)" #| Vol: $VOLUME
 else
 
-## Streaming Radio Players
+# VLC
+IFVLCON=`sh $HOME/scripts/system/check_vlc_running-PID.sh`
+if [ "$IFVLCON" = "On" ]; then
+    echo -e "►$(sh ~/scripts/video/nowplaying-vlc.sh)" #| Vol: $VOLUME
+else
+
+    ## Streaming Radio Players
 # Variables Shell-FM - radio
 #IFONLFM=`sh $HOME/scripts/system/check_shell-fm_running-PID.sh`
 if [ -f $HOME/.shell-fm/nowplaying ]; then
    IFONLFM=`sh $HOME/scripts/system/check_shell-fm_running-PID.sh`
    NPLFM=`cat $HOME/.shell-fm/nowplaying`
-   echo -e "♫ $NPLFM ♫ $VOLUME"
+   echo -e ":♪: $NPLFM" #| Vol: $VOLUME
 else
 # Variables Pianobar - radio
 IFONPB=`sh $HOME/scripts/system/check_pianobar_running-PID.sh`
-NPPB=`cat $HOME/.config/pianobarfly/nowplaying`
+NPPB=`cat $HOME/.config/pianobar/nowplaying`
    if [ "$IFONPB" = "On" ]; then
-      echo -e "♫ $NPPB ♫ $VOLUME"
+      echo -e ":♪: $NPPB" #| Vol: $VOLUME
 #    sleep 10
 #    wmiir remove /rbar/anowplaying
+else
+# Variables Pianobar fly - radio
+IFONPF=`sh $HOME/scripts/system/check_pianobarfly_running-PID.sh`
+NPPB=`cat $HOME/.config/pianobarfly/nowplaying`
+   if [ "$IFONPF" = "On" ]; then
+      echo -e ":♪: $NPPB" #| Vol: $VOLUME
+#    sleep 10
+#    wmiir remove /rbar/anowplaying
+else
+# Variables Spotify - radio
+IFONSPOTIFY=`sh $HOME/scripts/system/check_spotify_running-PID.sh`
+if [ "$IFONSPOTIFY" = "On" ]; then
+    echo -e ":♪: np: $(sh ~/bin/spotify-nowplaying)"
 else
 IFONMOC=`sh $HOME/scripts/system/check_moc_running-PID.sh`
 if [ "$IFONMOC" = "On" ]; then
@@ -42,9 +75,12 @@ STATE=$(mocp -i | grep State)
 ARTIST=$(mocp -i | grep Artist | cut -f2 -d ":")
 TITLE=$(mocp -i | grep SongTitle | cut -f2 -d ":")
 #    if [ "$STATE" == "State: PLAY" ]; then
-        echo -e "♫ $TITLE by $ARTIST ♫ $VOLUME"
+        echo -e ":♪: np: $TITLE by $ARTIST on $ALBUM" #| Vol: $VOLUME
     else
-        echo -e "-"
+        echo -e ":♭: x"
+    fi
+    fi
+fi
 fi
 fi
 fi
