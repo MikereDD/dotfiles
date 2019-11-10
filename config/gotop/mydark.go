c4049783b531fe04a29a613483cda166eaf25565
Mike Redd <mikeredd@protonmail.com>
2019-11-10 17:51:55 -0600

    add gotop theme  Changes to be committed: 	new file:   config/gotop/mydark.go 	new file:   config/gotop/mydark.json

---
+++ config/gotop/mydark.go
@@ -0,0 +1,25 @@
+package colorschemes
+
+var mydark = Colorscheme{
+	Fg: 15,
+	Bg: -1,
+
+	BorderLabel: 245,
+	BorderLine:  254,
+
+	CPULines: []int{81, 80, 148, 208, 178, 158, 255, 249},
+
+	BattLines: []int{81, 80, 148, 208, 178, 158, 255, 249},
+
+	MainMem: 51,
+	SwapMem: 122,
+
+	ProcCursor: 45,
+
+	Sparkline: 45,
+
+	DiskBar: 252,
+
+	TempLow:  40,
+	TempHigh: 196,
+}
